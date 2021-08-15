efb2e85c709a795b8737d4c90b49a58689a4f12b
Vicky Abishek <abishek.v@asg.com>
2021-08-15 19:22:25 +0530

    Add basic Layout n nav
    todo
    navigation direction
    proper ui

---
+++ app/src/main/java/com/udacity/shoestore/LoginFragment.kt
@@ -0,0 +1,31 @@
+package com.udacity.shoestore
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.findNavController
+import kotlinx.android.synthetic.main.fragment_login.*
+//import com.udacity.shoestore.databinding.LoginFragmentBinding
+//import com.vicky.android.navigation.databinding.FragmentGameWonBinding
+
+class LoginFragment : Fragment(R.layout.fragment_login) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+//        btn_signin.setOnClickListener { view: View ->
+//            view.findNavController()
+//                .navigate(LoginFragment.action_loginFragment_to_welcomeFragment)
+        }
+    }
+    //    override fun onCreateView(
+//        inflater: LayoutInflater, container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View? {
+//        // Inflate the layout for this fragment
+//        return inflater.inflate(R.layout.fragment_login, container, false)
+//    }
+
+}
