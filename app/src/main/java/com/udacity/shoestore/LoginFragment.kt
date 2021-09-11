ab74711003e5f233dd2d8f3e421a093e9813b696
Vicky Abishek <abishek.v@asg.com>
2021-09-11 21:18:31 +0530

    Incomplete - Backup

--- app/src/main/java/com/udacity/shoestore/LoginFragment.kt
+++ app/src/main/java/com/udacity/shoestore/LoginFragment.kt
@@ -10,22 +10,21 @@ import kotlinx.android.synthetic.main.fragment_login.*
 //import com.udacity.shoestore.databinding.LoginFragmentBinding
 //import com.vicky.android.navigation.databinding.FragmentGameWonBinding
 
-class LoginFragment : Fragment(R.layout.fragment_login) {
+class LoginFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
 
-//        btn_signin.setOnClickListener { view: View ->
-//            view.findNavController()
-//                .navigate(LoginFragment.action_loginFragment_to_welcomeFragment)
+        btn_signin.setOnClickListener { view: View ->
+            view.findNavController().navigate(R.id.action_loginFragment_to_welcomeFragment)
         }
     }
-    //    override fun onCreateView(
-//        inflater: LayoutInflater, container: ViewGroup?,
-//        savedInstanceState: Bundle?
-//    ): View? {
-//        // Inflate the layout for this fragment
-//        return inflater.inflate(R.layout.fragment_login, container, false)
-//    }
+        override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
 
 }
--- app/src/main/java/com/udacity/shoestore/MainActivity.kt
+++ app/src/main/java/com/udacity/shoestore/MainActivity.kt
@@ -2,13 +2,20 @@ package com.udacity.shoestore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import timber.log.Timber
+//import com.udacity.shoestore.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+//    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Timber.plant(Timber.DebugTree())
+        val navController = this.findNavController(R.id.nav_host_fragment)
+//        NavigationUI.setupActionBarWithNavController(this, navController)
     }
 }
